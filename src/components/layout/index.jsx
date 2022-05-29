066e10b0503c62ffb0b1e86ee223f35057af6b50
Lidianne Barbosa <lidiannerb@gmail.com>
2022-05-29 17:24:39 -0300

    feat:add logo heisenburger

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,12 +1,16 @@
 import "./style.css";
 import { Footer } from "../footer";
+import heisenburger from "../../assets/heisenburger.svg";
+
 export const LayoutForm = (props) => {
   return(
-    <section className="container">
+    <section className="container">      
         <div className="container-form">
-          <div className="wrap-form">
-              {props.children}
-
+          <div className="container-logo-heisenburger">
+            <img className="image-heisenburger" src={heisenburger} alt="Logo heisenburger"/>
+          </div>
+          <div className="wrap-form">            
+            {props.children}
           </div>
         </div>
         <Footer/>
