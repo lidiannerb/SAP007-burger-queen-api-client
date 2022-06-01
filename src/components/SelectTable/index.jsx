fd41337b76256a1979f9b2719c26ab6eb70c7e00
Lidianne Barbosa <lidiannerb@gmail.com>
2022-05-31 23:11:26 -0300

    feat: WIP/ style-menu
    Co-authored-by: TailaMartins <tailamartins74@gmail.com>
    Co-authored-by: lidiannerb <lidiannerb@gmail.com>

--- src/components/SelectTable/index.jsx
+++ src/components/SelectTable/index.jsx
@@ -3,10 +3,19 @@ const tables = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 export function SelectTable({onChange, value}) {
   return (
-      <select value={value} onChange={onChange}>
-          <option value ="" hidden >Mesa</option>
+      <select 
+        value={value} 
+        onChange={onChange}>
+        <option 
+          value ="" 
+          hidden >Mesa
+        </option>
           {tables.map(table=>
-             <option  key={table} value={table}>{table}</option>)}
+        <option  
+          key={table} 
+          value={table}
+        >{table}
+        </option>)}
         </select>
 
   );
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,29 +1,34 @@
 /* eslint-disable no-unused-vars */
 import { RiLogoutBoxRLine } from "react-icons/ri";
+import {GiHamburgerMenu} from "react-icons/gi";
 import Logo from "../../assets/heisenburger.svg";
 import { Button } from "../button";
 import "./style.css";
 
+
 export function Header({ className, onClick }){
   return (
     <header className={className}>
-      <ul className="header-container-ul">
-        <li className="image-heisenburger-container">
-          <img
-            className="image-heisenburger"
-            src={Logo}
-            alt="Logo heisenburger"
-          />
-        </li>
-        <li className="btn-logout-container">            
-          <Button
-            className="btn-logout"
-            onClick={onClick}
-          >
-            <RiLogoutBoxRLine className="icon" />
-          </Button>
-        </li>
-      </ul>
+      <Button
+        className="btn-menu-lateral"
+      >
+        <GiHamburgerMenu
+          className="icon"
+         />
+      </Button>        
+      <img
+        className="image-heisenburger"
+        src={Logo}
+        alt="Logo heisenburger"
+      />                 
+      <Button
+        className="btn-logout"
+        onClick={onClick}
+      >
+        <RiLogoutBoxRLine className="icon" />
+      </Button>
+        
+      
     </header>
   );
 }
